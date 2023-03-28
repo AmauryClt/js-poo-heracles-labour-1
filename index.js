5589f54258eb7f85f7d0fd5e3627c9d363a00076
Amaury CLOT <amauryclt@gmail.com>
2023-03-28 12:20:05 +0200

    hercule amaury

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 // First Labour : Heracles vs Nemean Lion
 // use your Figher class here
+
+const Fighter = require("./src/Fighter.js")
+
+
+const heracles = new Fighter('Heracles', 20, 6);
+const nemeanLion = new Fighter('Nemean Lion', 11, 13);
+
+
+console.log(heracles, nemeanLion);
+console.log(attack);
+
--- src/Fighter.js
+++ src/Fighter.js
@@ -1,1 +1,21 @@
 /* Fighter class definition */
+
+const MAX_LIFE = 100;
+
+class Fighter {
+    constructor(nom, force, dexteriter, life) {
+        this.nom = nom;
+        this.force = force;
+        this.dexteriter = dexteriter;
+        this.life = MAX_LIFE;
+    }
+    fight(defender) {
+        const attack = Math.floor(Math.random() * this.force + 1);
+        const mitigationAttack = Math.max(0, attack - this.dexteriter);
+        const lifeRest = Math.max(this.life - mitigationAttack);
+        defender.life = lifeRest;
+        return (`${this.name} attaque ${defender.name} en infligeant ${mitigationAttack} point de dégâts. ${defender.name} a maintenant ${defender.life} points de vie.`);
+    }
+}
+
+module.exports = Fighter
